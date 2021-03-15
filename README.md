40f42c0bf674df3d527c045a3054fd4616caca58
Sergey Bovyrin <sbovyrin@gmail.com>
2021-03-15 20:13:34 +0000

    Release: v1.0.0

---
+++ README.md
@@ -0,0 +1,41 @@
+# Validator
+
+Describe schema of your data and then parse input data using described schema.
+
+## Getting started
+
+```php
+<?php
+
+\Validator::parse(DATA_SCHEMA)($input);
+```
+
+Where:
+- `DATA_SCHEMA` describes rules for input parsing
+- `$input` some data from one of app's forms
+
+
+### Schema possible format
+
+```php
+<?php
+
+SCHEMA = [
+  'name' => ['type' => 'text', 'required' => true, 'max' => 64],
+  'email' => ['type' => 'email', 'required' => true, 'max' => 128],
+  'age' => ['type' => 'number', 'required' => false, 'min' => 18, 'max' => 100],
+  'active' => ['type' => 'boolean', 'required' => true],
+  'country' => [
+    'type' => 'text',
+    'required' => true,
+    'in' => ['USA', 'Canada']
+  ],
+  // also valid empty rules
+  'attribute' => []
+];
+```
+
+- type: `text`|`number`|`boolean`|`email`
+- required: `true`|`false`
+- min|max: applies for number or length of string
+- in: list of possible values
---
+++ src/Validator.php
@@ -0,0 +1,81 @@
+<?php declare(strict_types=1);
+
+
+class Validator
+{
+    static function rule(string $name): callable
+    {
+        return [
+            'required' => fn ($v, array $rules) => 
+                !is_null($v) || !$rules['required'],
+            'type' => function ($v, array $rules) {
+                switch ($rules['type']) {
+                    case 'text':
+                        return is_string($v);
+                    case 'number':
+                        return is_numeric($v);
+                    case 'boolean':
+                        return $v == 1 || $v == 0;
+                    case 'email':
+                        return is_string($v);
+                }
+            },
+            'min' => function ($v, array $rules) {
+                switch ($rules['type']) {
+                    case 'text':
+                        return !is_null($v) && strlen($v) >= $rules['min'];
+                    case 'email':
+                        return !is_null($v) && strlen($v) >= $rules['min'];
+                }
+            },
+            'max' => function ($v, array $rules) {
+                switch ($rules['type']) {
+                    case 'text':
+                        return !is_null($v) && strlen($v) <= $rules['max'];
+                    case 'email':
+                        return !is_null($v) && strlen($v) <= $rules['max'];
+                }
+            },
+            'in' => fn ($v, array $rules) => in_array($v, $rules['in']),
+            'pattern' => fn ($v, array $rules) => 
+                (bool) !is_null($v) && preg_match("/{$rules['pattern']}/", $v)
+        ][$name];
+    }
+
+    // TODO: need refactoring
+    static function parse(array $rules): callable
+    {
+        return fn (array $data): array => array_reduce(
+            array_keys($rules),
+            function ($acc, $attr) use ($data, $rules) {
+
+                // if not required skip parsing
+                if (!isset($data[$attr]) && !($rules[$attr]['required'] ?? false)) {
+                    return $acc;
+                }
+
+                $errs = array_reduce(
+                    array_keys($rules[$attr]),
+                    function ($_errs, string $name) use ($attr, $data, $rules) {
+                        return array_merge(
+                            $_errs,
+                            self::rule($name)($data[$attr] ?? null, $rules[$attr])
+                                ? []
+                                : [$name => $rules[$attr][$name]]
+                        );
+                    },
+                    []
+                );
+
+                if (empty($errs)) $acc['vals'][$attr] = $data[$attr];
+                else $acc['errs'] = array_merge(
+                    $acc['errs'] ?? [],
+                    [$attr => $errs]
+                );
+
+                return $acc;
+            },
+            []
+        );
+    }
+}
