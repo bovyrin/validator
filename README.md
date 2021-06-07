ca3d47c104ce5867ce52b69b5f667514e9a27bf1
Sergey Bovyrin <sbovyrin@gmail.com>
2021-06-08 00:07:14 +0300

    Update: refactor inner implementation

--- README.md
+++ README.md
@@ -1,43 +1,34 @@
 # Validator
 
-Describe schema of your data and then parse input data using described schema.
+Describe rules of your data and then parse input data using described your rules.
 
-## Getting started
+## Getting started (WIP)
 
 ```php
 <?php
 
 use sbovyrin\Validator;
 
-Validator::parse(SCHEMA)($input);
+// $rules looks like:
+[
+    'attr1': ['type' => 'text', 'required' => true, 'max' => 16],
+    'attr2': ['type' => 'number', 'required' => true, 'max' => 100, 'min' => 1],
+    'attr3': ['type' => 'boolean', 'required' => false],
+    'attr4': ['type' => 'number', 'required' => true, 'in' => [1,2,3,4]],
+    'attr5': ['type' => 'number', 'required' => true, 'pattern' => '[0-5]']
+]
+
+// $data looks like:
+[
+    'attr1' => 'Hello',
+    'attr2' => 5,
+    'attr3' => true,
+    'attr4' => 3,
+    'attr5' => 2
+]
+
+$validateDataByTheRules = Validator::parse($rules);
+$validatedData = $validateDataByTheRules($data);
+// or
+$validatedData = Validator::parse($rules)($data);
 ```
-
-Where:
-- `SCHEMA` describes rules for input parsing
-- `$input` some data from one of app's forms
-
-
-### Schema possible format
-
-```php
-<?php
-
-SCHEMA = [
-  'name' => ['type' => 'text', 'required' => true, 'max' => 64],
-  'email' => ['type' => 'email', 'required' => true, 'max' => 128],
-  'age' => ['type' => 'number', 'required' => false, 'min' => 18, 'max' => 100],
-  'active' => ['type' => 'boolean', 'required' => true],
-  'country' => [
-    'type' => 'text',
-    'required' => true,
-    'in' => ['USA', 'Canada']
-  ],
-  // also valid empty rules
-  'attribute' => []
-];
-```
-
-- type: `text`|`number`|`boolean`|`email`
-- required: `true`|`false`
-- min|max: applies for number or length of string
-- in: list of possible values
--- src/Validator.php
+++ src/Validator.php
@@ -1,87 +1,119 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace sbovyrin;
 
 
 class Validator
 {
-    static function rule(string $name): callable
+    private static function required($val, bool $isRequired): bool
     {
-        return [
-            'required' => fn ($v, array $rules) => 
-                !is_null($v) || !$rules['required'],
-            'type' => function ($v, array $rules) {
-                switch ($rules['type']) {
-                    case 'text':
-                        return is_string($v);
-                    case 'number':
-                        return is_numeric($v);
-                    case 'boolean':
-                        return $v == 1 || $v == 0;
-                    case 'email':
-                        return is_string($v);
-                }
-            },
-            'min' => function ($v, array $rules) {
-                switch ($rules['type']) {
-                    case 'number':
-                        return !is_null($v) && $v >= $rules['min'];
-                    case 'text':
-                        return !is_null($v) && strlen($v) >= $rules['min'];
-                    case 'email':
-                        return !is_null($v) && strlen($v) >= $rules['min'];
-                }
-            },
-            'max' => function ($v, array $rules) {
-                switch ($rules['type']) {
-                    case 'number':
-                        return !is_null($v) && $v <= $rules['max'];
-                    case 'text':
-                        return !is_null($v) && strlen($v) <= $rules['max'];
-                    case 'email':
-                        return !is_null($v) && strlen($v) <= $rules['max'];
-                }
-            },
-            'in' => fn ($v, array $rules) => in_array($v, $rules['in']),
-            'pattern' => fn ($v, array $rules) => 
-                (bool) !is_null($v) && preg_match("/{$rules['pattern']}/", $v)
-        ][$name];
+        if ($isRequired) return !is_null($val);
+        return true;
     }
 
-    // TODO: need refactoring
-    static function parse(array $rules): callable
+    private static function type($val, string $type): bool
     {
-        return fn (array $data): array => array_reduce(
-            array_keys($rules),
-            function ($acc, $attr) use ($data, $rules) {
+        switch ($type) {
+            case 'file':
+            case 'email':
+            case 'text': return is_string($val);
+            case 'number': return is_numeric($val);
+            case 'boolean': return $val == 1 || $val == 0;
+        }
 
-                // if not required skip parsing
-                if (!isset($data[$attr]) && !($rules[$attr]['required'] ?? false)) {
-                    return $acc;
-                }
+        return false;
+    }
 
-                $errs = array_reduce(
-                    array_keys($rules[$attr]),
-                    function ($_errs, string $name) use ($attr, $data, $rules) {
-                        return array_merge(
-                            $_errs,
-                            self::rule($name)($data[$attr] ?? null, $rules[$attr])
-                                ? []
-                                : [$name => $rules[$attr][$name]]
-                        );
-                    },
-                    []
-                );
+    private static function min($val, int $min): bool
+    {
+        switch (true) {
+            case self::type($val, 'email'):
+            case self::type($val, 'file'):
+            case self::type($val, 'text'): return strlen($val) >= $min;
+            case self::type($val, 'number'): return $val >= $min;
+        }
 
-                if (empty($errs)) $acc['vals'][$attr] = $data[$attr];
-                else $acc['errs'] = array_merge(
-                    $acc['errs'] ?? [],
-                    [$attr => $errs]
-                );
+        return false;
+    }
 
-                return $acc;
-            },
-            []
+    private static function max($val, int $max): bool
+    {
+        switch (true) {
+            case self::type($val, 'email'):
+            case self::type($val, 'file'):
+            case self::type($val, 'text'): return strlen($val) <= $max;
+            case self::type($val, 'number'): return $val <= $max;
+        }
+
+        return false;
+    }
+
+    private static function in($val, array $vals): bool
+    {
+        return in_array($val, $vals);
+    }
+
+    private static function pattern($val, string $pattern): bool
+    {
+        return (bool) preg_match("/{$pattern}/", $val);
+    }
+
+    /**
+     * Parse $data structure using $rules
+     *
+     * @Example
+     * $rules looks like:
+     * [
+     *      'attr1': ['type' => 'text', 'required' => true, 'max' => 16],
+     *      'attr2': ['type' => 'number', 'required' => true, 'max' => 100, 'min' => 1],
+     *      'attr3': ['type' => 'boolean', 'required' => false],
+     *      'attr4': ['type' => 'number', 'required' => true, 'in' => [1,2,3,4]],
+     *      'attr5': ['type' => 'number', 'required' => true, 'pattern' => '[0-5]']
+     * ]
+     *
+     * $data looks like:
+     * [
+     *      'attr1' => 'Hello',
+     *      'attr2' => 5,
+     *      'attr3' => true,
+     *      'attr4' => 3,
+     *      'attr5' => 2
+     * ]
+     *
+     * $validateDataByTheRules = Validator::parse($rules);
+     * $validatedData = $validateDataByTheRules($data);
+     * or
+     * $validatedData = Validator::parse($rules)($data);
+     */
+    static function parse(array $rules, array $dflt = []): callable
+    {
+        $_parse = static fn ($v, $_rules): array => array_map(
+            static fn ($p, $x) => self::$p($v, $x)
+                ? [$v, []]
+                : [$v, [$p => $x]],
+            array_keys($_rules),
+            $_rules
         );
+
+        $_output = static fn (array $res): array => [
+            'val' => $res[0][0],
+            'errs' => array_merge(...array_column($res, 1))
+        ];
+
+        return static fn ($data):array =>
+            array_reduce(
+                array_keys($rules),
+                static fn ($a, $b) => array_merge(
+                    $a,
+                    [
+                        $b => $_output(
+                            $_parse($data[$b] ?? $dflt[$b] ?? null, $rules[$b])
+                        )
+                    ]
+                ),
+                []
+            );
     }
 }
